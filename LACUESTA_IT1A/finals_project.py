6e2519e583abd1c59a3661577dd7db7def1efed0
zedricklloyd <zedricklacuesta@gmail.com>
2024-12-11 23:36:52 +0800

    Update finals_project.py

--- LACUESTA_IT1A/finals_project.py
+++ LACUESTA_IT1A/finals_project.py
@@ -30,7 +30,7 @@ def act20():
                 print()
             continue    
 
-def codechalleng2():
+def codechalleng5():
     prelim = eval(input("Enter your Prelim grade ---->"))
     midterm = eval(input("Enter your Midterm grade----->"))
     semifinal = eval(input("Enter your Semi - final grade -->"))
@@ -242,4 +242,33 @@ def codechallenge3():
     
     print ("\n Hi, My name is" , fullname , "and I'm" , str(age) , "years old" , gender , " born on" , birthdate, "\n My marital status is" , maritalStatus , "\n I'm " , religion , "\n I'm" , nationality ,  "My height is" , height , "cm" " and I'm" , weight , "kl" , "\n My number is" , contactnumber , "\n You may also contact me through my e-mail as" , email , "\n I live in" , address , "\n My father is" , fathersname , "and my mother is" , mothersname , "\n I love" , hobby )
 
-    
+def codechallenge5():
+    print("=======================================================================================================")
+
+fahrenheit = eval(input("Enter temperature in Fahrenheit - - - >  "))
+
+c = ((fahrenheit - 32 ) * 5 ) / 9
+
+print(f"{fahrenheit} degrees Fahrenheit converted to celcius is {round(c, 2)} degrees")
+
+def codechallenge6():
+    prelim = eval(input("Enter your Prelim grade ---->"))
+    midterm = eval(input("Enter your Midterm grade----->"))
+    semifinal = eval(input("Enter your Semi - final grade -->"))
+    final = eval(input("Enter your final grade --->"))
+    quiz = eval(input("Enter your quiz grade -->"))
+    project = eval(input("Enter your project grade ------->"))
+
+    if (prelim >= 65 and prelim <= 100) and (midterm >= 65 and midterm <= 100) and (semifinal >= 65 and semifinal <= 100) and (final >= 65 and final <= 100) and (quiz >= 65 and quiz <= 100) and (project >= 65 and project <= 100):
+        FG=(prelim * .15) + (midterm * .15) + (semifinal * .15) + (final * .15) + (quiz * .25) + (project * .15)
+
+        print(f"\nYour final grade is: {round(FG , 2)}")
+
+        if FG >=75:
+            print("Congratulations! You passed the course ")
+
+        else:
+            print("Sorry you failed")
+
+    else:
+        print("Invalid Grades")
