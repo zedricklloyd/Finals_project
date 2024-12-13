42a531f0979c2348b4963049ed8f77adee61aba4
zedricklloyd <zedricklacuesta@gmail.com>
2024-12-13 16:12:00 +0800

    Create activity8.py

---
+++ LACUESTA_IT1A/activity8.py
@@ -0,0 +1,11 @@
+password = input("Enter your password: ")
+
+if password.lower() == "bsit":
+    print("Acces granted")
+    print("Enjoy using the system")
+
+elif password.lower() == "it":
+    print("Enjoy using the system")
+
+else:
+    print("Access denied")
