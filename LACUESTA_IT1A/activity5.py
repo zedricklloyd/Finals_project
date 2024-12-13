9c2af082a2be2279fb06bcf976a3af49f6e8f2c6
zedricklloyd <zedricklacuesta@gmail.com>
2024-12-13 16:12:05 +0800

    Create activity5.py

---
+++ LACUESTA_IT1A/activity5.py
@@ -0,0 +1,6 @@
+print("FAHRENHEIT TO CELCIUS CONVERTER PROGRAM")
+print(" ==================================== ")
+
+fahrenheit = eval(input("Enter a number: "))
+celcius = ((fahrenheit - 32) * 5) / 9
+print(f"{fahrenheit} degree fahrenheit converter to celcius is {celcius} degrees.")
