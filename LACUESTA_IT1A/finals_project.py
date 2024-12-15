a6080d04701c94be419ebb95103b77d84c377420
zedricklloyd <zedricklacuesta@gmail.com>
2024-12-15 23:13:10 +0800

    Update finals_project.py

--- LACUESTA_IT1A/finals_project.py
+++ LACUESTA_IT1A/finals_project.py
@@ -1,16 +1,183 @@
-from activity2 import act2
-from activity4 import act4
-from activity5 import act5
-from activity6 import act6
-from activity7 import act7
-from activity8 import act8
-from activity9 import act9
-from activity10 import act10
-from activity11 import act11
-from activity12 import act12
-from activity13 import act13
-from activity14 import act14
+import os
+def body():
+    print(f"\nWelcome to code compilation of Zedrick Lloyd Lacuesta")
+    print(f"BSIT-1A")
+    print(f"\nChoose what action you want to do:")
+    print(f"\n1. Open Activity Folder")  
+    print(f"2. Open Code Challenge Folder")
+    print(f"3. Exit Program")
 
+
+def act1():
+    print("Hello World")
+
+def act2():
+    name = input("What is your name?  ")
+    print("Hi,", name)
+
+def act4():
+    number1 = eval(input("Enter number: "))
+    number2 = eval(input("Enter number: "))
+
+    sum = number1 + number2
+    print(f"The sum of number1 and number2 is {sum}.")
+
+def act5():
+    print("FAHRENHEIT TO CELCIUS CONVERTER PROGRAM")
+    print(" ==================================== ")
+
+    fahrenheit = eval(input("Enter a number: "))
+    celcius = ((fahrenheit - 32) * 5) / 9
+    print(f"{fahrenheit} degree fahrenheit converter to celcius is {celcius} degrees.")
+
+def act6():
+    x = 5
+    print(x)
+
+    x+=10
+    print(x)
+
+    x+=15
+    print(x)
+
+    x+=30
+    print(x)
+
+def act7():
+    gold = 0
+    name = input("Hi, Please enter your full name: ")
+
+    isGold = input("is the mineral gold? ")
+
+    #if yes is not in lower case the program will then proceed to the else statement
+
+    if isGold.lower() == "yes":
+        gold+=1
+        print(f"\nHi {name}, Your total number of gold is {gold}")
+    else:
+        print(f"Hi {name}, Your total number of gold is {gold}")
+
+def act8():
+    password = input("Enter your password: ")
+
+    if password.lower() == "bsit":
+        print("Acces granted")
+        print("Enjoy using the system")
+
+    elif password.lower() == "it":
+        print("Enjoy using the system")
+
+    else:
+        print("Access denied")
+
+def act9():
+    age = eval(input("Enter your age ----->"))
+
+    if age >=1 and age <=7:
+        print("\nYour age is categorized as Toddler")
+
+    elif age >=8 and age <=13:
+        print("\nYour age is categorized as Pre-teen")
+
+    elif age >=14 and age <=18:
+        print("\nYour age is categorized as Teenager ")
+
+    elif age >=19 and age <=31:
+        print("\nYour age is categorized as Early Adulthood ")
+
+    elif age >=32 and age <=45:
+        print("\nYour age is categorized as Mid Adulthood ")
+
+    elif age >=46 and age <=59:
+        print("\nYour age is categorized as Post Adulthood ")
+
+    elif age >=60 and age <=90:
+        print("\nYour age is categorized as Senior ")
+
+def act10():
+    name = input("Enter your name: ")
+    isStudent = input("are your current student of DLL? (yes or No) -->")
+
+    if isStudent.lower() == "yes":
+        yearlevel = input("\n f - Freshman \n s - sophomore \n j - Junior \n r - Senior \n what is your current level right now in DLL? ")
+        if yearlevel.lower() == "f":
+            print(f"Hi {name} you are Freashman")
+        elif yearlevel.lower() == "s":
+            print(f"Hi {name} you are sophomore ")
+        elif yearlevel.lower() == "j":
+            print(f"Hi {name} you are junior")
+        elif yearlevel.lower() == "r":
+            print(f"Hi {name} you are senior")
+        else:
+            print("Invalid choice")
+
+def act11():
+    for x in range(1,10):
+        print (x, "Goodbye IT DePARTMENT")
+
+def act12():
+    for x in range(10,0,-1):
+        print(x)
+
+def act13():
+    sum = 1
+    num = eval(input("Enter a number: "))
+
+    for x in range(num, 0, -1):
+        sum *= x
+    print(f"the factorial of {num} is {sum}")
+
+def act14():
+    for x in range(0,11):
+        print(x, end = " ")
+    for y in range(0,11):
+        print("*", end = " ")
+    print()
+act14()
+
+def act15():
+    for x in range(0,11):
+        print(x, end =" ")
+    for y in range(0, x):
+        print("*", end= " ")
+    print()
+
+def act16():
+    for x in range(1,11):
+        for z in range(1, x+1):
+            print(" ", end= " ")
+        for y in range(11, x,-1):
+            print("*", end= " ")
+        print()
+
+def activity17():
+    col= eval(input("Enter nummber of column --> "))
+    for x in range(1,11):
+        for y in range(1,col+1):
+          print(f"{x} x {y} = {x*y}",end= "\t")
+        print()
+
+def act18():
+    ngi=eval(input("How many triangles you want to create? "))
+
+    for x in range(1,6):
+        for y in range(1,ngi+1):
+            for z in range(1,x+1):
+                print("*", end= " ")
+            for a in range(5,x,-1):
+                print(" ", end= " ")
+            print( end= " ")
+        print()
+               
+def activity19():
+    tuloy = True
+    while tuloy == True:
+        name = input("Enter your name: ")
+        if  name.lower()== "stop":
+            print("Program Terminated")
+            break
+        else:
+            continue
 
 def act20():
     import os
@@ -35,7 +202,111 @@ def act20():
                 print()
             continue    
 
-def codechalleng5():
+def act21():
+    tuloy = True
+    count = 0
+
+    while tuloy == True:
+        name = input("Please enter a name(type 'end' to stop the loop ) ---> ")
+            
+        if name.lower() == "end":
+            print("Loop has now stopped")
+            print(f"You have entered {count} names")
+            break
+            tuloy = False
+        else:
+            count += 1
+
+def act22():
+    def acti22():
+        def activity1():
+            print("Hello World")
+        activity1()
+    acti22()
+
+def act23():
+    def factorial(number):
+        """ This function's purpose is to compute/calculate the factorial of any number given """
+        fact = 1
+        for x in range(number, 0, -1):
+            fact *= x
+
+        return fact
+
+    print(f"THe factorial of 13 is {factorial(13)}")
+
+def act24():
+    from activity23 import factorial
+
+    print(f"the factorial of 7 is {factorial(7)} ")
+
+def act25():
+    courses = ["BSIT", "BSA", "BSAIS", "BTVTED", "BSSW", "BSPA", "Delete w/o index", "Delete with index"]
+
+    courses.remove("Delete w/o index")
+    courses.pop()
+    courses.append("DHRS")
+    courses.insert(0, "ABELS")
+
+    print(courses)
+
+def cc1():
+    print("\t\t\t\t\t\t\t\t\t\t\t  * \n\t\t\t\t\t\t\t\t\t\t\t *** \n\t\t\t\t\t\t\t\t\t\t\t***** \n\t\t\t\t\t\t\t\t\t\t       ******* \n\t\t\t\t\t\t\t\t\t\t\t***** \n\t\t\t\t\t\t\t\t\t\t\t ***\n\t\t\t\t\t\t\t\t\t\t\t  *")
+
+def cc2():
+    name = input("Enter your name - - - > ")
+    print("\t\t\t\t\t\t\t\t\t\t\t       * \n\t\t\t\t\t\t\t\t\t\t\t    * * * * \n\t\t\t\t\t\t\t\t\t\t\t  * * * * * * \n\t\t\t\t\t\t\t\t\t\t       * Hi!," , name ,"* \n\t\t\t\t\t\t\t\t\t\t\t  * * * * * * \n\t\t\t\t\t\t\t\t\t\t\t    * * * * \n\t\t\t\t\t\t\t\t\t\t\t       *")
+
+def cc3():
+    fullname = input("Please type your full name: ")
+    age = input("Please type your age: ")
+    gender = input("Please type your gender: ")
+    birthdate = input ("Please type your birth date: ")
+    maritalStatus = input("Please type your marital status: ")
+    religion = input("Please type your religion: ")
+    nationality = input("Please type your nationality: ")
+    height = input("Please type your height in cm: ")
+    weight = input("Please type your weight in kl: ")
+    contactnumber = input("Please type your contact number: ")
+    email = input ("Please type your e-mail: ")
+    address = input("Please type your home address: ")
+    fathersname = input("Please type your father's full name: ")
+    mothersname = input("Please type your mother's full name: ")
+    hobby = input ("Please type your hobby: ")
+
+    
+    print ("\n Hi, My name is" , fullname , "and I'm" , str(age) , "years old" , gender , " born on" , birthdate, "\n My marital status is" , maritalStatus , "\n I'm " , religion , "\n I'm" , nationality ,  "My height is" , height , "cm" " and I'm" , weight , "kl" , "\n My number is" , contactnumber , "\n You may also contact me through my e-mail as" , email , "\n I live in" , address , "\n My father is" , fathersname , "and my mother is" , mothersname , "\n I love" , hobby )
+
+def cc4():
+    n1= eval(input("Enter a number - - > "))
+    n2= eval(input("Enter a number - - > "))
+
+    ans1 = n1 + n2
+    ans2 = n1 - n2
+    ans3 = n1 * n2
+    ans4 = n1 // n2
+    ans5 = n1 / n2
+    ans6 = n1 % n2
+    ans7 = n1 ** n2
+
+    print("\nThe sum of " , n1 , "and" , n2 , "is" , ans1)
+    print("The difference of " , n1 , "and" , n2 , "is" , ans2) 
+    print("The product of " , n1 , "and" , n2 , "is" , ans3) 
+    print("The floor division of " , n1 , "and" , n2 , "is" , ans4) 
+    print("The quotient of " , n1 , "and" , n2 , "is" , ans5) 
+    print("The remainder of " , n1 , "and" , n2 , "is" , ans6) 
+    print(n1 , "exponent by " , n2 ,"is" , ans7) 
+
+def cc5():
+    print("=======================================================================================================")
+
+    fahrenheit = eval(input("Enter temperature in Fahrenheit - - - >  "))
+
+    c = ((fahrenheit - 32 ) * 5 ) / 9
+
+    print(f"{fahrenheit} degrees Fahrenheit converted to celcius is {round(c, 2)} degrees")
+
+def cc6():
     prelim = eval(input("Enter your Prelim grade ---->"))
     midterm = eval(input("Enter your Midterm grade----->"))
     semifinal = eval(input("Enter your Semi - final grade -->"))
@@ -43,18 +314,62 @@ def codechalleng5():
     quiz = eval(input("Enter your quiz grade -->"))
     project = eval(input("Enter your project grade ------->"))
 
-    FG=(prelim * .15) + (midterm * .15) + (semifinal * .15) + (final * .15) + (quiz * .25) + (project * .15)
+    if (prelim >= 65 and prelim <= 100) and (midterm >= 65 and midterm <= 100) and (semifinal >= 65 and semifinal <= 100) and (final >= 65 and final <= 100) and (quiz >= 65 and quiz <= 100) and (project >= 65 and project <= 100):
+        FG=(prelim * .15) + (midterm * .15) + (semifinal * .15) + (final * .15) + (quiz * .25) + (project * .15)
 
-    print(f"\nYour final grade is: {round(FG , 2)}")
+        print(f"\nYour final grade is: {round(FG , 2)}")
 
-    if FG >=101:
-        print("Invalid input")
+        if FG >=75:
+            print("Congratulations! You passed the course ")
 
-    elif FG >=75:
-        print("Congratulations! You passed the course ")
+        else:
+            print("Sorry you failed")
 
     else:
-        print("Sorry you failed")
+        print("Invalid Grades")
+
+def cc7():
+    grocery = input("Did you buy a grocery? (yes/no):  ")
+    if grocery.lower() == "yes":
+        name = input("Name of the item:  ")
+        price = eval(input("Price of the item:  "))
+        age = eval(input("Age:  "))
+        amount = eval(input("Amount Given: "))
+        
+        tax = 0.123
+        taxamount = price * tax
+        total = price + taxamount
+
+        if age <= 59:
+            change = amount - total
+
+            print(f"\nHi customer, you purchased a {name} , with a price of {price} plus 12.3 % tax ({taxamount}) total of {total} ")
+            print(f"Amount Given: {amount}php")
+            print(f"Change : {round(change , 2)}  ")
+
+
+        if age >= 60:
+            discount = 0.052
+            disamount = price * 0.052
+            distotal = price - disamount
+            change1 = amount - distotal
+
+            print(f"\nHello customer, you paid {price} for a {name} with a senior discount of 5.2 % ({disamount}). The total amount you paid is {round(distotal , 2)} ")
+            print(f"Amount Given: {amount}php")
+            print(f"Change : {round(change1 , 2)}  ")
+
+    else:
+        print("No grocery purchased")
+	
+def cc8():
+    sum = 0
+    for u in range(1, 11):
+        num = int(input(f"Enter number {u} :"))
+        sum += num
+        
+    print(f"The summation of all provided number is {sum}")
+
+
 
 def codechallenge10():
     for x in range(1,6):
@@ -78,40 +393,6 @@ def codechallenge10():
         for y in range(6,x,-1):
             print("*", end= " ")
         print()
-
-def denomination():
-    name = input("Enter your name: ")
-    deposit = eval(input("Enter the amount to deposit: "))
-
-    n1 = deposit // 1000
-    n11 = deposit - (n1 * 1000 )
-
-    n2 = n11 // 500
-    n22 = n11 % 500
-
-    n3 = n22 // 200
-    n33 = n22 % 200
-
-    n4 = n33 // 100
-    n44 = n33 % 100
-
-    n5 = n44 // 50
-    n55 = n44 % 50
-
-    n6 = n55 // 20
-    n66 = n55 % 20
-
-    n7 = n66 // 10
-    n77 = n66 % 10
-
-    n8 = n77 // 5
-    n88 = n77 % 5
-
-    n9 = n88 // 1
-    n99 = n88 % 1
-
-    print("\n Hi" , name , "your deposit exchange in PH denomination are as follow: ")
-    print("\n" , n1 ,"-  1000" , "\n" , n2 , "-  500" , "\n" , n3 , "-  200" , "\n" , n4 , "-  100" , "\n" , n5 , "-  50" , "\n" , n6 , "-  20" , "\n" , n7 , "-  10" , "\n" , n8 , "-  5" , "\n" , n9 , "-  1")
 
 
 def codechallenge16():
@@ -227,82 +508,37 @@ def codechallenge16():
             print("Invalid pick. Please try again.")
             continue
 
-def codechallenge3():
-    fullname = input("Please type your full name: ")
-    age = input("Please type your age: ")
-    gender = input("Please type your gender: ")
-    birthdate = input ("Please type your birth date: ")
-    maritalStatus = input("Please type your marital status: ")
-    religion = input("Please type your religion: ")
-    nationality = input("Please type your nationality: ")
-    height = input("Please type your height in cm: ")
-    weight = input("Please type your weight in kl: ")
-    contactnumber = input("Please type your contact number: ")
-    email = input ("Please type your e-mail: ")
-    address = input("Please type your home address: ")
-    fathersname = input("Please type your father's full name: ")
-    mothersname = input("Please type your mother's full name: ")
-    hobby = input ("Please type your hobby: ")
 
-    
-    print ("\n Hi, My name is" , fullname , "and I'm" , str(age) , "years old" , gender , " born on" , birthdate, "\n My marital status is" , maritalStatus , "\n I'm " , religion , "\n I'm" , nationality ,  "My height is" , height , "cm" " and I'm" , weight , "kl" , "\n My number is" , contactnumber , "\n You may also contact me through my e-mail as" , email , "\n I live in" , address , "\n My father is" , fathersname , "and my mother is" , mothersname , "\n I love" , hobby )
+def denomination():
+    name = input("Enter your name: ")
+    deposit = eval(input("Enter the amount to deposit: "))
 
-def codechallenge5():
-    print("=======================================================================================================")
+    n1 = deposit // 1000
+    n11 = deposit - (n1 * 1000 )
 
-fahrenheit = eval(input("Enter temperature in Fahrenheit - - - >  "))
+    n2 = n11 // 500
+    n22 = n11 % 500
 
-c = ((fahrenheit - 32 ) * 5 ) / 9
+    n3 = n22 // 200
+    n33 = n22 % 200
 
-print(f"{fahrenheit} degrees Fahrenheit converted to celcius is {round(c, 2)} degrees")
+    n4 = n33 // 100
+    n44 = n33 % 100
 
-def codechallenge6():
-    prelim = eval(input("Enter your Prelim grade ---->"))
-    midterm = eval(input("Enter your Midterm grade----->"))
-    semifinal = eval(input("Enter your Semi - final grade -->"))
-    final = eval(input("Enter your final grade --->"))
-    quiz = eval(input("Enter your quiz grade -->"))
-    project = eval(input("Enter your project grade ------->"))
+    n5 = n44 // 50
+    n55 = n44 % 50
 
-    if (prelim >= 65 and prelim <= 100) and (midterm >= 65 and midterm <= 100) and (semifinal >= 65 and semifinal <= 100) and (final >= 65 and final <= 100) and (quiz >= 65 and quiz <= 100) and (project >= 65 and project <= 100):
-        FG=(prelim * .15) + (midterm * .15) + (semifinal * .15) + (final * .15) + (quiz * .25) + (project * .15)
+    n6 = n55 // 20
+    n66 = n55 % 20
 
-        print(f"\nYour final grade is: {round(FG , 2)}")
+    n7 = n66 // 10
+    n77 = n66 % 10
 
-        if FG >=75:
-            print("Congratulations! You passed the course ")
+    n8 = n77 // 5
+    n88 = n77 % 5
 
-        else:
-            print("Sorry you failed")
+    n9 = n88 // 1
+    n99 = n88 % 1
 
-    else:
-        print("Invalid Grades")
-
- 
-from activity23 import act23
-def factorial(number):
-    """ This function's purpose is to compute/calculate the factorial of any number given """
-    fact = 1
-    for x in range(number, 0, -1):
-        fact *= x
-
-    return fact
-
-print(f"THe factorial of 13 is {factorial(13)}")
-
-def activity19():
-    tuloy = True
-    while tuloy == True:
-        name = input("Enter your name: ")
-        if  name.lower()== "stop":
-            print("Program Terminated")
-            break
-        else:
-            continue
-
-def activity17():
-    col= eval(input("Enter nummber of column --> "))
-    for x in range(1,11):
-        for y in range(1,col+1):
-          print(f"{x} x {y} = {x*y}",end= "\t")
-        print()
+    print("\n Hi" , name , "your deposit exchange in PH denomination are as follow: ")
+    print("\n" , n1 ,"-  1000" , "\n" , n2 , "-  500" , "\n" , n3 , "-  200" , "\n" , n4 , "-  100" , "\n" , n5 , "-  50" , "\n" , n6 , "-  20" , "\n" , n7 , "-  10" , "\n" , n8 , "-  5" , "\n" , n9 , "-  1")
